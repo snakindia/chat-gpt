b99944daa2775a177d2c6c701b480189ad8e07a8
Rahul Sharma <rahuls@snakconsultancy.com>
2023-04-04 19:15:13 +0530

    main updated with some design changes

--- pages/SpeechToText.tsx
+++ pages/SpeechToText.tsx
@@ -5,41 +5,6 @@ import { toast } from 'react-hot-toast'
 import { useState } from 'react';
 import axios from 'axios'
 
-// const SpeechToText = () => {
-
-//   const {
-//     recording,
-//     speaking,
-//     transcribing,
-//     transcript,
-//     pauseRecording,
-//     startRecording,
-//     stopRecording,
-//   } = useWhisper({
-//     apiKey: process.env.NEXT_PUBLIC_OPENAI_API_KEY // YOUR_OPEN_AI_TOKEN
-//   })
-
-//   return (
-//         <div className='flex items-center justify-center h-screen flex-col bg-black'>
-//              <h1 className='text-white mb-2px font-bold text-2xl mb-8'>ChatGPT Voice Recognition</h1>
-//             <div className="flex items-center mb-8">
-//             <button 
-//                 className='micStart w-12 h-12 mr-2 outline-none cursor-pointer rounded bg-gray-500' 
-//                 onClick={() =>startRecording()} 
-//                 style={{ background: recording ? '#e3bfbc' : '#9db0b8' }}>
-//                     <FiberManualRecord style={{ color: 'red' }}/>
-//             </button>
-//             <button 
-//                 className='micStop w-12 h-12 mr-2 outline-none cursor-pointer rounded bg-gray-500' 
-//                 onClick={() => stopRecording()}><Stop />
-//             </button>
-//             </div>
-//         {recording && <CircularProgress />}
-//         <span className='w-6/12 text-center text-white'>{transcript.text}</span>
-//         </div>
-//   )
-// }
-
 const SpeechToText = () => {
   const [showModal, setShowModal] = useState(false);
 
@@ -57,15 +22,10 @@ const SpeechToText = () => {
 
   const handleModalClose = () => {
     setShowModal(false);
+    setTranscription("")
   };
 
-  // const handleFileUpload = (event) => {
-  //   // handle the file upload logic here
-  //   console.log(event.target.files[0]);
-  //   setShowModal(false);
-  // };
   const API_ENDPOINT = "https://api.openai.com/v1/audio/transcriptions";
-
   
   const handleFormSubmit = async (event: any) => {
     setLoader(true)
@@ -109,26 +69,37 @@ const SpeechToText = () => {
 
   return (
     <>
-      <button onClick={handleButtonClick}>Upload File</button>
         {showModal && (
           <div className="modal">
             <div className="modal-content">
               <span className="close" onClick={handleModalClose}>
                 &times;
               </span>
-              <h2>Select File to Upload</h2>
-              <form onSubmit={handleFormSubmit}>
-                <input type="file" accept="audio/*" onChange={handleFileChange} />
-                <button type="submit">Transcribe</button>
-              </form>
+              <div className='selection-div'>
+                <h2 className='modal-hdg'>Select File to Upload</h2>
+                <form onSubmit={handleFormSubmit}>
+                  <input type="file" accept="audio/*" onChange={handleFileChange} />
+                  <button type="submit">Transcribe</button>
+                </form>
+              </div>
               {loader && <CircularProgress />}
-              {transcription}
+              {/* <div className='divider'>
+                  OR
+              </div> */}
+              <div className='transcription'>
+                {transcription}
+              </div>
+              {/* <div className='recording'>
+                <button onClick={() =>startRecording()}>Start recording</button>
+              </div> */}
             </div>
+            
           </div>
         )}
 
          <div className='flex items-center justify-center h-screen flex-col bg-black'>
               <h1 className='text-white mb-2px font-bold text-2xl mb-8'>ChatGPT Voice Recognition</h1>
+              <button onClick={handleButtonClick} className='text-white mb-2px font-bold text-2xl mb-8'>Click Me to Upload File</button>
              <div className="flex items-center mb-8">
              <button 
                  className='micStart w-12 h-12 mr-2 outline-none cursor-pointer rounded bg-gray-500' 
